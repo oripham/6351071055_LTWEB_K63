1a1cf7c7254b4c53fcfcc3380ea6268e3b07531b
Oanh Phạm <opham414@gmail.com>
2024-11-01 17:04:19 +0700

    init

---
+++ WebBanSach/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanSach.Models;
+
+namespace WebBanSach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private readonly QLBANSACHEntities _context = new QLBANSACHEntities();
+
+
+        public ActionResult Index()
+        {
+            var chudeList = _context.CHUDEs.ToList();
+            var nhaxuatbanList = _context.NHAXUATBANs.ToList();
+            var sachList = _context.SACHes.ToList();
+
+            ViewBag.chudeList = chudeList;
+            ViewBag.nhaxuatbanList = nhaxuatbanList;
+            ViewBag.sachList = sachList;
+
+            return View();
+        }
+
+        public ActionResult ChuDe(int maCD=0)
+        {
+            var sachList = _context.SACHes
+                            .Where(s => s.MaCD == maCD)
+                            .ToList();
+
+            ViewBag.title = _context.CHUDEs
+                        .Where(s => s.MaCD == maCD)
+                        .Select(s => s.TenChuDe)
+                        .FirstOrDefault();
+
+            ViewBag.sachList = sachList;
+            ViewBag.chudeList = _context.CHUDEs.ToList();
+            ViewBag.nhaxuatbanList = _context.NHAXUATBANs.ToList();
+            return View("ChuDe");
+        }
+
+        public ActionResult NhaXuatBan(int maNXB=0)
+        {
+            var sachList = _context.SACHes
+                            .Where(s => s.MaNXB == maNXB)
+                            .ToList();
+
+            ViewBag.title = _context.NHAXUATBANs
+                        .Where(s => s.MaNXB == maNXB)
+                        .Select(s => s.TenNXB)
+                        .FirstOrDefault();
+
+            ViewBag.sachList = sachList;
+            ViewBag.chudeList = _context.CHUDEs.ToList();
+            ViewBag.nhaxuatbanList = _context.NHAXUATBANs.ToList();
+
+            return View("NhaXuatBan");
+        }
+
+        public ActionResult Detail(int id=0) 
+        {
+            ViewBag.chudeList = _context.CHUDEs.ToList();
+            ViewBag.nhaxuatbanList = _context.NHAXUATBANs.ToList();
+
+            var book = _context.SACHes.Where(s => s.Masach == id).FirstOrDefault();
+            ViewBag.book = book;
+
+            return View("Detail");
+        }
+    }
+}
